c197e853667e28d661a238daae5a99aa9504571e
Ankit Tyagi <you@example.com>
2021-11-06 02:22:19 +0530

    apkbuild

--- app/build/generated/source/buildConfig/release/io/github/ankittyagi206/twa/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/io/github/ankittyagi206/twa/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "io.github.ankittyagi206.twa";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "4";
 }
