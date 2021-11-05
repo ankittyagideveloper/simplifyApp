f6e99dc02842ec97bcafb6d0c5257fc82057bff5
Ankit Tyagi <you@example.com>
2021-11-05 17:31:30 +0530

    apkbuild

---
+++ app/src/main/java/io/github/ankittyagi206/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.ankittyagi206.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
